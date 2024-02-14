42e19c79c69a88b7e8aad2c975cefbd1a7d17f83
Bhargav Sojitra <bhargavsojitra009@gmail.com>
2024-02-14 19:50:39 +0530

    add register and debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,69 @@
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloud } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+const registerUser = asyncHandler(async (req, res) => {
+  // get user detail from frontend
+  //validation
+  //check if already exist : username , email
+  //images , avatar
+  // upload them  to cloudinary
+  // create object - create entry db
+  // remove password and refrash token feild
+  //check for user creation
+  //return res
 
-import {asyncHandler}  from '../utils/asyncHandler.js';
-const registerUser = asyncHandler(async(req,res) => {
-    res.status(200).json({
-        message:"ok"
-    })
-})
+  const { fullName, email, username, password } = req.body;
 
+  if (
+    [fullName, email, username, password].some((feild) => feild?.trim() === "")
+  ) {
+    throw new ApiError(400, "All feild required");
+  }
 
-export {registerUser};
+  const existedUser = User.findOne({ $or: [{ email }, { username }] });
+
+  if (existedUser) {
+    throw new ApiError(409, "Email or Username Already Exist");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is mendetory");
+  }
+
+  //because the files are big like 5mb 7mb than await for the upload
+  const avatar = await uploadOnCloud(avatarLocalPath);
+  const coverImage = await uploadOnCloud(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is mendetory");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    //hear we check the coverImage validation because of we not check erlier coverIamege exists
+    coverImage: coverImage ? coverImage.url : "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUSer = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (createdUSer) {
+    throw new ApiError(500, "Server Error");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUSer, "Created user Successfully"));
+});
+
+export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import { upload } from "../middlewaers/multer.middlewaers.js";
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-
+router.route("/register").post(
+  upload.fields([
+    //avatar , images
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
