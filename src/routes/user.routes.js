70f2b05f4deaf5714d1e57a6017a5a48a72e3a48
Bhargav Sojitra <bhargavsojitra009@gmail.com>
2024-02-16 17:28:03 +0530

    new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { upload } from "../middlewaers/multer.middlewaers.js";
 import {
+  changeCurrentPassword,
+  currentUser,
+  getUserChannalProfile,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccount,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewaers/auth.middlewaer.js";
 
@@ -30,5 +35,15 @@ router.route("/login").post(loginUser);
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, currentUser);
+router.route("/update-account").patch(verifyJWT, updateAccount);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+router.route("/channal/:username").get(verifyJWT, getUserChannalProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
