077cfad8d0905e9beb0d92b63d78f9aa7281d8e6
Bhargav Sojitra <bhargavsojitra009@gmail.com>
2024-02-16 17:17:48 +0530

    wathch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloud } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // hear userid came from to the user because the username and password are correct
 const genrateAccessAndRefreshToken = async (userId) => {
@@ -254,7 +255,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 });
 
 const currentUser = asyncHandler(async (req, res) => {
-  return res.status(200).json(200, req.user, "current user fatch successfully");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "current user fatch successfully"));
 });
 
 const updateAccount = asyncHandler(async (req, res) => {
@@ -332,6 +335,130 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover Image update "));
 });
 
+const getUserChannalProfile = asyncHandler(async (req, res) => {
+  const { userName } = req.params;
+
+  if (!userName?.trim()) {
+    throw new ApiError(400, "userName is missing");
+  }
+
+  const channal = await User.aggregate([
+    {
+      $match: {
+        userName: userName?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channal",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subcribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subsribersCount: {
+          $size: "$subscribers",
+        },
+        channalsSubsribedToCount: {
+          $size: "$subcribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: (req.user?._id, "$subscribers.subscriber") },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subsribersCount: 1,
+        channalsSubsribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  console.log(channal);
+  if (!channal?.length) {
+    throw new ApiError(404, "channal does not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channal[0], "user channal fatched succesfully"));
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        " Watch history fetched successfull"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -342,4 +469,7 @@ export {
   updateAccount,
   updateAvatar,
   updateCoverImage,
+  channal,
+  getUserChannalProfile,
+  getWatchHistory,
 };
